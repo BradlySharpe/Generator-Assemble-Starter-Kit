30d9ccd7495e9bc9b0672b654a49c25a85906f75
Bradly Sharpe <Bradly@tailormadesolutions.com.au>
2015-10-10 12:40:31 +1100

    Fix default task

--- generators/app/templates/grunt/tasks/default.js
+++ generators/app/templates/grunt/tasks/default.js
@@ -1,3 +1,3 @@
 module.exports = function(grunt) {
-  grunt.registerTask('default', ['assemble', 'compass', 'postcss']);
+  grunt.registerTask('default', ['assemble', 'compass', 'postcss', 'connect', 'watch']);
 };
