51f8dc675b912770be3694e59b1f7166bf4a9983
Bradly Sharpe <Bradly@tailormadesolutions.com.au>
2015-10-10 12:25:43 +1100

    Add config helper

--- generators/app/templates/src/config/helpers/helpers.js
+++ generators/app/templates/src/config/helpers/helpers.js
@@ -16,4 +16,38 @@ module.exports.register = function(Handlebars, options) {
   Handlebars.registerHelper('json', function(context) {
     return JSON.stringify(context);
   });
+
+  Handlebars.registerHelper('config', function(path, options) {
+    var attrs = {
+        "lowercase": false,
+        "prefix": "",
+        "sufix": ""
+      },
+      json = require('grunt').file.readJSON('./package.json'),
+      keys = path.split('.'),
+      next = keys.shift();
+
+    for (var prop in options.hash) {
+      if (attrs.hasOwnProperty(prop))
+        attrs[prop] = options.hash[prop];
+    }
+
+    function getValue(json, property, keys) {
+      if (json.hasOwnProperty(property)) {
+        var prop = json[property];
+        if (0 < keys.length) {
+          if ("object" === typeof prop) {
+            var next = keys.shift();
+            return getValue(prop, next, keys);
+          }
+        } else {
+          var output = attrs.prefix + prop + attrs.sufix;
+          return (attrs.lowercase) ? output.toLowerCase() : output;
+        }
+      }
+      return null;
+    }
+
+    return getValue(json, next, keys);
+  });
 };
